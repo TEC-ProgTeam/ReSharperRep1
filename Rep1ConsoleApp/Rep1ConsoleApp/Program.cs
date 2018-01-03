229fb85848e9ecb819eee592064f6226270dbea7
TEC-joel <joel@tec.dk>
2018-01-03 13:47:20 +0100

    Initial Commit console app

---
+++ Rep1ConsoleApp/Rep1ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rep1ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
